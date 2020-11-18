61ea22f3719e8e807193e4f5310716c63d8bb391
azolayza <azovcevae@gmail.com>
2020-11-18 23:51:34 +0700

    lesson7
    edit of articlePageObject for ios

--- src/test/java/lib/ui/ios/iOSArticlePageObject.java
+++ src/test/java/lib/ui/ios/iOSArticlePageObject.java
@@ -6,7 +6,7 @@ import lib.ui.ArticlePageObject;
 public class iOSArticlePageObject extends ArticlePageObject {
     static {
         TITLE = "id:Java (programming language)";
-        FOOTER_ELEMENT = "id:View article in browser]";
+        FOOTER_ELEMENT = "id:View article in browser";
         OPTIONS_ADD_TO_MY_LIST = "id:Save for later";
         ADD_TO_MY_LIST_OVERLAY = "id:org.wikipedia:id/onboarding_button";
         CLOSE_ARTICLE_BUTTON = "id:Back";
--- src/test/java/lib/ui/ios/iOSSearchPageObject.java
+++ src/test/java/lib/ui/ios/iOSSearchPageObject.java
@@ -11,7 +11,7 @@ public class iOSSearchPageObject extends SearchPageObject
         SEARCH_INIT_ELEMENT = "id:Search Wikipedia";
         SEARCH_INPUT = "xpath://XCUIElementTypeApplication[@name=\"Wikipedia\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeSearchField";
         SEARCH_CANCEL_BUTTON = "id:Close";
-        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeStaticText[contains(@name,'{SUBSTRING}']";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeStaticText[contains(@name,'{SUBSTRING}')]";
         //XCUIElementTypeStaticText[@name="Object-oriented programming language"]
         SEARCH_RESULT_ELEMENT = "xpath://XCUIElementTypeCell";
         SEARCH_EMPTY_RESULT_LABEL ="xpath://XCUIElementTypeStaticText[@name='No results found']";
